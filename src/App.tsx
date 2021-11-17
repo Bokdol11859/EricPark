48048af8f186e59fbc52d27d1b4220ce990787ed
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2021-11-18 01:07:41 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,20 @@ function App() {
       <section>
         <img src="/images/1.png" data-speed="-2" className="layer" />
         <img src="/images/3.png" data-speed="-2" className="layer" />
-        <img src="/images/2.png" data-speed="6" className="layer" />
-        <img src="/images/4.png" data-speed="-3" className="layer" />
+        <img src="/images/2.png" data-speed="1" className="layer" />
+        <img src="/images/4.png" data-speed="-2" className="layer" />
         <img src="/images/5.png" data-speed="1" className="layer" />
-        <img src="/images/6.png" data-speed="-3" className="layer" />
-        <img src="/images/7.png" data-speed="-6" className="layer" />
-        <img src="/images/8.png" data-speed="-5" className="layer" />
+        <img src="/images/6.png" data-speed="-2" className="layer" />
+        <img src="/images/7.png" data-speed="-1" className="layer" />
+        <img src="/images/8.png" data-speed="-1" className="layer" />
         <img src="/images/9.png" data-speed="-1" className="layer" />
         <img src="/images/10.png" data-speed="-3" className="layer" />
         <img src="/images/11.png" data-speed="-2" className="layer" />
-        <img src="/images/12.png" data-speed="-4" className="layer" />
-        <h2 className="layer">Hello</h2>
+        <img src="/images/12.png" data-speed="-2" className="layer" />
+        <div className="Description">
+          <h2>My Name is Eric Park</h2>
+          <h4>A Passionate Front-End Developer</h4>
+        </div>
       </section>
     </div>
   );
