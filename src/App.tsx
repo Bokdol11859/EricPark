dbc24141311ab9cf8f4863085a6594debbef5aba
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2021-11-18 00:50:07 +0900

    Parallax movement implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div className="App">
-      <h1>BokDol Blog</h1>
-      <p>Created for Personal Portfolio</p>
-      <p>Test</p>
+      <section>
+        <img src="/images/1.png" data-speed="-2" className="layer" />
+        <img src="/images/3.png" data-speed="-2" className="layer" />
+        <img src="/images/2.png" data-speed="6" className="layer" />
+        <img src="/images/4.png" data-speed="-3" className="layer" />
+        <img src="/images/5.png" data-speed="1" className="layer" />
+        <img src="/images/6.png" data-speed="-3" className="layer" />
+        <img src="/images/7.png" data-speed="-6" className="layer" />
+        <img src="/images/8.png" data-speed="-5" className="layer" />
+        <img src="/images/9.png" data-speed="-1" className="layer" />
+        <img src="/images/10.png" data-speed="-3" className="layer" />
+        <img src="/images/11.png" data-speed="-2" className="layer" />
+        <img src="/images/12.png" data-speed="-4" className="layer" />
+        <h2 className="layer">Hello</h2>
+      </section>
     </div>
   );
 }
